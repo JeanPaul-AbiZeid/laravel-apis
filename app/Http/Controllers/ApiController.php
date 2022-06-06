1e78861e880690b0c2c7d06ffb8ab7795ee07ed0
jean paul <jeanpaulabizeid@gmail.com>
2022-06-06 15:10:14 +0300

    creating a function to calculate the time passed since 1732

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1,10 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    
+    public function getTime(){
+        $current_date = date(time()); //since 1/1/1970
+        $years = 237*365*24*60*60; //years from 1732 tp 1970
+        $months = 8*30*24*60*60; //months from april till january
+        $days = 16*24*60*60; //days from 14 till 1
+        
+        $time_to_add = $years + $months + $days;
+        $seconds_passed = $current_date + $time_to_add;
+        
+        return response()->json([
+            "status" => "Success",
+            "result" => $seconds_passed
+        ], 200);
+    } 
 }
--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
 
+Route::get('/time', [ApiController::class, 'getTime']);
 
